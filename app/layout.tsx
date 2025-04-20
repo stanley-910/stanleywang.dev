fb32de577cb63cc891cf15765991c2d74cdebcb9
twinfantasyfan <wangstanley910@gmail.com>
2025-04-20 15:42:14 -0400

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,20 +19,20 @@ export const viewport: Viewport = {
 }
 
 export const metadata: Metadata = {
-  title: 'Nim - Personal website template',
+  title: 'Stanley Wang',
   description:
-    'Nim is a free and open-source personal website template built with Next.js 15, React 19 and Motion-Primitives.',
+    'Stanley Wang - Site',
 }
 
-const geist = Geist({
-  variable: '--font-geist',
-  subsets: ['latin'],
-})
+// const geist = Geist({
+//   variable: '--font-geist',
+//   subsets: ['latin'],
+// })
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+// const geistMono = Geist_Mono({
+//   variable: '--font-geist-mono',
+//   subsets: ['latin'],
+// })
 
 export default function RootLayout({
   children,
@@ -44,11 +44,6 @@ export default function RootLayout({
       <body
         className="bg-white tracking-tight antialiased dark:bg-zinc-950"
       >
-      {/* <body
-        className={`          ${geist.variable} ${geistMono.variable} 
-        bg-white tracking-tight antialiased dark:bg-zinc-950`}
-      > */}
-        {/* This is the theme provider, which allows the user to switch between light and dark mode */}
         <ThemeProvider
           enableSystem={true}
           attribute="class"
@@ -56,7 +51,7 @@ export default function RootLayout({
           defaultTheme="system"
         >
           <div className="flex min-h-screen w-full flex-col">
-            <div className="relative mx-auto w-full max-w-screen-sm flex-1 px-4 pt-20 flex flex-col">
+            <div className="relative mx-auto w-full max-w-[65ch] flex-1 px-4 pt-20 flex flex-col">
               <BackgroundArt type="random" />
               <Header />
               <main className="flex-1">
