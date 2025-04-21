0aa43f8bb3eaa3e82aad4bd8cbd9755721abe132
twinfantasyfan <wangstanley910@gmail.com>
2025-04-21 02:05:05 -0400

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from 'next'
+import { Analytics } from "@vercel/analytics/react"
 // import { Geist, Geist_Mono } from 'next/font/google'
 // Font styles
 import '@fontsource/dm-mono'
